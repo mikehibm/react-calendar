b57d6f58e3c720d9ec8adea4f409dff92061471a
Makoto Ishida <makotoishida@gmail.com>
2015-12-12 07:55:19 +0000

    Refactor App component. Separate CalendarHeader component.

--- js/app.jsx
+++ js/app.jsx
@@ -1,69 +1,80 @@
 const React = require('react'),
-      ReactDOM = require('react-dom'),
-      Router = require('director').Router,
-      Store = require('./store.jsx'),
-      Actions = require('./actions.jsx');
+  ReactDOM = require('react-dom'),
+  Router = require('director').Router,
+  Store = require('./store.jsx'),
+  Actions = require('./actions.jsx');
 
 /* global moment */
 
-const App = React.createClass({
-  getInitialState: function() {
-    return Store.getState();
-  },
-  
-  componentDidMount: function () {
-    Store.addChangeListener(this._onChange);
-  },
-
-  componentWillUnmount: function () {
-    Store.removeChangeListener(this._onChange);
-  },
-  
-  prevMonth: function(){
-    console.log('Prev Month');
+class CalendarHeader extends React.Component {
+  prevMonth() {
     Actions.prevMonth();
-  },
-  nextMonth: function(){
-    console.log('Next Month');
+  }
+  nextMonth() {
     Actions.nextMonth();
-  },
+  }
+
+  render() {
+    const { year, month } = this.props;
+
+    return <header>
+          <button onClick={ this.prevMonth.bind(this) }>&lt;</button>
+          { year }年 { month }月
+          <button onClick={ this.nextMonth.bind(this) }>&gt;</button>
+        </header>;
+  }
+}
+
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = Store.getState();
+  }
   
-  render: function() {
+  componentDidMount() {
+    Store.addChangeListener(this._onChange.bind(this));
+  }
+
+  componentWillUnmount() {
+    Store.removeChangeListener(this._onChange.bind(this));
+  }
+
+  render() {
     const currentDate = this.state.currentDate;
     const year = moment(currentDate).year();
     const month = moment(currentDate).month() + 1;
     const dayOfMonth = moment(currentDate).date();
     const startOfMonth = moment(currentDate).startOf('month');
     const startDay = startOfMonth.weekday();
-    console.log('startDay=' + startDay);
-    
-    const days = [], dayHeader = [];
+
+    const days = [],
+      dayHeader = [];
     const dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-    
-    dayNames.forEach(function(item, index){
+
+    dayNames.forEach(function(item, index) {
       dayHeader.push(<div key={ index }>{ item }</div>);
     });
-    
-    for (let i = 0; i < startDay; i++){
+
+    for (let i = 0; i < startDay; i++) {
       days.push(<div key={i} className={'past day ' + dayNames[ i % 7 ]}>&nbsp;</div>);
     }
-    
-    for (let i = 0 + startDay; i < 31 + startDay; i++){
-      let isToday = moment().isSame(moment({year: year, month: month-1, day: (i - startDay+1)}), 'day');
+
+    for (let i = 0 + startDay; i < 31 + startDay; i++) {
+      let isToday = moment().isSame(moment({
+        year: year,
+        month: month - 1,
+        day: (i - startDay + 1)
+      }), 'day');
       let dayClass = isToday ? " today" : "";
       days.push(<div key={i} className={'day ' + dayNames[ i % 7 ] + dayClass}>{ i - startDay +1 }</div>);
     }
-    
-    let items = this.state.items.map(function(item){
+
+    let items = this.state.items.map(function(item) {
       return <li key={ item.id }>{ item.text }</li>
     });
     return (
       <section>
-        <header>
-          <button onClick={ this.prevMonth }>&lt;</button>
-          { year}年 { month }月
-          <button onClick={ this.nextMonth }>&gt;</button>
-        </header>
+        <CalendarHeader year={ year } month={ month }></CalendarHeader>
         
         <div className='dayHeader'>
           { dayHeader }
@@ -78,25 +89,33 @@ const App = React.createClass({
         </div>
       </section>
     );
-  },
-  
-  _onChange: function () {
+  }
+
+  _onChange() {
     this.setState(Store.getState());
-  }  
-});
+  }
+}
 
 
 //Define routes.
 var router = Router({
-  '/': function(){ Actions.showAll(); },
-  '/active': function(){ Actions.showActive(); },
-  '/completed': function(){ Actions.showCompleted(); }
-})
-.configure({
-  'notfound': function(){ Actions.showAll(); },
-  'html5history': false
-})
-.init();
+    '/': function() {
+      Actions.showAll();
+    },
+    '/active': function() {
+      Actions.showActive();
+    },
+    '/completed': function() {
+      Actions.showCompleted();
+    }
+  })
+  .configure({
+    'notfound': function() {
+      Actions.showAll();
+    },
+    'html5history': false
+  })
+  .init();
 
 
 ReactDOM.render(
